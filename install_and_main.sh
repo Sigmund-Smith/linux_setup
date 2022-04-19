179486c1ee1cbf72fa36405a89f63b3813c1ffe3
Sigmund-Smith <github@leanderraoul.de>
2022-04-19 15:18:49 +0200

    first prototype with installable programs and vimrc setup

--- install_and_main.sh
+++ install_and_main.sh
@@ -2,53 +2,43 @@
 
 echo "yo " # declare beginning of script with a cool exclamation
 
-# mylist="hi das steht hire so"
+mylist="htop neofetch vim firefox"
 yes_answer="y"
 
 
 function install_function {
-    for thing in ${mylist[@]}; do 
+    for thing in ${mylist[@]}; do
 
         # next is the real shit
-        ###apt install $thing
-        ###echo "another one done \n"
+        apt install $thing
+        echo
+        echo "+++++ another one done +++++"
+        echo
 
-        # for testing this one is better 
-        echo $thing
+        # for testing this one is better
+        #echo $thing
     done
 }
 
-function setup_vimrc {
-    echo ":syntax on" >> ~/.vimrc    
-    echo ":set relavitvenumber" >> ~/.vimrc    
-    echo ":set number" >> ~/.vimrc    
-    echo ":inoremap üü <Esc>" >> ~/.vimrc    
-    echo "" >> ~/.vimrc
-    echo ':set expandtab:set tabstop=4     " Size of a hard tabstop (ts).' >> ~/.vimrc 
-    echo ':set shiftwidth=4  " Size of an indentation (sw).' >> ~/.vimrc
-    echo ':set expandtab     " Always uses spaces instead of tab characters (et).' >> ~/.vimrc
-    echo ':set softtabstop=0 " Number of spaces a <Tab> counts for. When 0, featuer is off (sts).' >> ~/.vimrc
-    echo ':set autoindent    " Copy indent from current line when starting a new line.' >> ~/.vimrc
-    echo ':set smarttab      " Inserts blanks on a <Tab> key (as per sw, ts and sts).' >> ~/.vimrc
-    echo "" >> ~/.vimrc    
-    echo "" >> ~/.vimrc    
+function call_pythonfile {
+    python3 manage_rc_files.py
 }
-
 
 #echo -n "Do you want to install things (y/n): "; read answer
 read -p "Do you want to install things? (y/n): " answer
 
-if [[ "$yes_answer" == "$answer" ]]; then 
-    echo "yöt es hat geklappt"
+if [[ "$yes_answer" == "$answer" ]]; then
+    echo "yöt, istalling things"
     install_function
+    echo "the installing has ended"
 
 fi
 
 read -p "Do you want to setup vimrc (~/.vimrc)? (y/n): " vim_answer
 
 if [[ "$yes_answer" == "$vim_answer" ]]; then
-    setup_vimrc
-    echo " vimrc has been set up "
+    call_pythonfile
+    echo "vimrc has been set up "
 fi
 
 echo "yo, done"
--- mangage_rc_files.py
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/python3
-
-#this file should be called by the programm installer/main file
-#this file should set upt the vimrc and bash rc file 
